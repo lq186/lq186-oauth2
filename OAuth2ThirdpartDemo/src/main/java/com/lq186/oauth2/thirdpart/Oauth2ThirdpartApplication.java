1b8a660432320043dbd3449044b2a268ac4fca2b
liuqing <liuqing@sinochain.com>
2019-03-14 17:49:49 +0800

    完善项目

---
+++ OAuth2ThirdpartDemo/src/main/java/com/lq186/oauth2/thirdpart/Oauth2ThirdpartApplication.java
@@ -0,0 +1,13 @@
+package com.lq186.oauth2.thirdpart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ThirdpartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ThirdpartApplication.class, args);
+    }
+
+}
